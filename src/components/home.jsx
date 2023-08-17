ddfcfac11409c87f0c1390a816fd6b51c3766918
Alfredo Alejandro Arguinzones <arguinzones.alejandro@gmail.com>
2023-08-17 11:31:28 -0400

    added: home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,22 @@
-function Home() {
+import { Link } from "react-router-dom";
+function Home({ authenticated, userName }) {
   return (
     <>
-      <h1>Home</h1>
+      <div className="container-center">
+        <h1 id="welcomeToHomePage">Welcome to Stanger&apos;s Things</h1>
+        {authenticated ? (
+          <>
+            <h3 id="userNameHomePage">Logged in as {userName}</h3>
+            <Link className="sendMessageButton loginButton" to="/logout">
+              Logout
+            </Link>
+          </>
+        ) : (
+          <Link className="sendMessageButton loginButton" to="/login">
+            Login
+          </Link>
+        )}
+      </div>
     </>
   );
 }
